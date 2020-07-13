607c59da87f069de12dd3c5a02e7798f209715bb
udv-code <dv4code@gmail.com>
2020-07-13 11:55:22 +0300

    Fixed IMGUI_API macro

--- imgui.h
+++ imgui.h
@@ -67,7 +67,11 @@ Index of this file:
 // IMGUI_API is used for core imgui functions, IMGUI_IMPL_API is used for the default bindings files (imgui_impl_xxx.h)
 // Using dear imgui via a shared library is not recommended, because we don't guarantee backward nor forward ABI compatibility (also function call overhead, as dear imgui is a call-heavy API)
 #ifndef IMGUI_API
-#define IMGUI_API
+#ifdef imgui_EXPORTS
+#define IMGUI_API __declspec(dllexport)
+#else
+#define IMGUI_API __declspec(dllimport)
+#endif
 #endif
 #ifndef IMGUI_IMPL_API
 #define IMGUI_IMPL_API              IMGUI_API
