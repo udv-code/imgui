fddcb41068c94095ca0c2b3ce1ffd4bb7288edf0
Dmitry Vasyliev <dv4code@gmail.com>
2021-01-13 12:37:31 +0200

    MInor improvements

--- examples/example_glfw_opengl3/main.cpp
+++ examples/example_glfw_opengl3/main.cpp
@@ -4,8 +4,8 @@
 // Read online: https://github.com/ocornut/imgui/tree/master/docs
 
 #include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
 #include <stdio.h>
 
 // About Desktop OpenGL function loaders:
